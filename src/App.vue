887b31cc1520e5cf962b94b3d09a2a634a4dc125
scenaristeur <scenaristeur@gmail.com>
2022-11-02 23:11:02 +0100

    load tables

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-        <router-link to="/about">TOUT</router-link>
     </nav>
     <router-view/>
+    0.0.1
   </div>
 </template>
 
