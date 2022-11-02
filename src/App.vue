f0cf70ae9becf6d8c1d503ae6036bc3a98e2436d
scenaristeur <scenaristeur@gmail.com>
2022-11-02 21:41:49 +0100

    noos

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+        <router-link to="/about">TOUT</router-link>
     </nav>
     <router-view/>
   </div>
